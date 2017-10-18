1807bd8975149746b7e2ac9edaaba4087fdaa8a0
MauricioAcosta <andres.taamap@gmail.com>
2017-10-17 22:54:35 -0500

    ordenando en carpeta uva

--- Power_of_Cryptography/cyptography.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin
-import math
-def calcula(n,p):
-    return math.pow(p,1/n)
-lectura=stdin.read().splitlines()
-while(lectura!= []):
-    n = lectura.pop(0)
-    p = lectura.pop(0)
-    print(round(calcula(int(n),int(p))))
