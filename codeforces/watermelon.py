b402fc4fc13609191d163b6bf0419d0a0b4ab750
MauricioAcosta <andres.taamap@gmail.com>
2017-11-03 14:31:12 -0500

    Accepted :ok_hand:

---
+++ codeforces/watermelon.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+watermelokg= int(stdin.readline());
+
+if watermelokg>=1 and watermelokg<=100:
+    if watermelokg % 2 == 0:
+        part=watermelokg/2
+        if part>1:
+            print('YES')
+        else:
+            print('NO')
+    else:
+        print('NO')
