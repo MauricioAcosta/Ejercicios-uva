e570302cd6e01bb9daca92a646c7c3b6e3f8c84c
Andres Acosta <andres.taamap@gmail.com>
2017-11-17 14:59:08 -0500

    fail T_T

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <>
+
+using namespace std;
+
+int main(){
+  int problemas, ficha, filas, columnas;
+  cin<<problemas;
+  for(int i=0; i<=problemas; i++){
+    cin<<ficha<<filas<<columnas;
+    cout>>ficha;
+  }
+}
+
+int Rook(int filas, int columnas){
+  return 0;
+}
+
+int Queen(){
+  return 0;
+}
+
+int King(){
+
+}
